3d5c10bdd9c52449d9dd840281ba9af334327f3e
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-08 16:08:39 +0530

    trial for kafka

--- paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/ParagraphTokenizerApplication.java
+++ paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/ParagraphTokenizerApplication.java
@@ -1,12 +1,25 @@
 package com.stackroute.knowledgevault.paragraphtokenizer;
 
+import com.stackroute.knowledgevault.paragraphtokenizer.kafka.Producer;
+import com.stackroute.knowledgevault.paragraphtokenizer.model.KvMessage;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParagraphTokenizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParagraphTokenizerApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner(final Producer producer) {
+		return (args) -> {
+			for (int i = 1; i < 2; i++) {
+				producer.send(new KvMessage(i, "A simple test message"));
+			}
+		};
 	}
 }
