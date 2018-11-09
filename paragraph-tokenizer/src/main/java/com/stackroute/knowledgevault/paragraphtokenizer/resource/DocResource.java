ff58a7704f6ec33df37b77625a2b04a08a240653
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-09 14:25:29 +0530

    updated parent version

--- paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/resource/DocResource.java
+++ paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/resource/DocResource.java
@@ -1,18 +1,28 @@
 package com.stackroute.knowledgevault.paragraphtokenizer.resource;
 
 import com.stackroute.knowledgevault.paragraphtokenizer.model.Document;
+import com.stackroute.knowledgevault.paragraphtokenizer.service.ParaTokenizerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("kafka")
 public class DocResource {
 
     @Autowired
     private KafkaTemplate<String, Document> kafkaTemplate;
 
+    @Autowired
+    private ParaTokenizerImpl paraTokenizer;
+
     private static final String topic = "paraTokens";
 
+    @GetMapping("/publish/{name}")
     public String post() {
-        kafkaTemplate.send(topic, new Document(2, "sdgasgdaisg"));
+        kafkaTemplate.send(topic, paraTokenizer.);
         return "published successfully";
     }
 }
