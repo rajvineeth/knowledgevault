6c83638bb25ec514bf3b6ad213c89ff80f7b451f
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-13 16:13:00 +0530

    kafka and input

---
+++ input-processor/src/main/java/com/stackroute/knowledgevault/inputprocessor/model/Input.java
@@ -0,0 +1,12 @@
+package com.stackroute.knowledgevault.inputprocessor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Input {
+    private String text;
+}
