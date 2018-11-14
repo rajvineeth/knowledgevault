18acff60a4be75b4c4fd02eaa70e16f308079cef
kushagraagrawal <kushagra106@gmail.com>
2018-11-14 15:09:35 +0530

    renamed NLPService to document-identifier

--- document-identifier/src/test/java/com/stackroute/knowledgevault/DocumentIdentifierApplicationTests.java
+++ document-identifier/src/test/java/com/stackroute/knowledgevault/DocumentIdentifierApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NlpServiceApplicationTests {
+public class DocumentIdentifierApplicationTests {
 
 	@Test
 	public void contextLoads() {
