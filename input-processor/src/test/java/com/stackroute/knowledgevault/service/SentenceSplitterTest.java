be6abcd5d73b966b91356a5e05e3f0698f060d0d
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-14 17:05:20 +0530

    used nlp in it

--- input-processor/src/test/java/com/stackroute/knowledgevault/service/SentenceSplitterTest.java
+++ input-processor/src/test/java/com/stackroute/knowledgevault/service/SentenceSplitterTest.java
@@ -28,12 +28,12 @@ public class SentenceSplitterTest {
     public void splitDocument() {
         Input input = new Input("My name is Shubham Dutta. I am 24 years old. I have an acute pain in my stomach accompanied by dysentery. There is pain in my lower back-bone. I've also vomited a couple of times. I had a 100F fever last night, so I took paracitamol to bring the fever down.");
         List list = new ArrayList();
-        list.add(new Sentence("My name is Shubham Dutta."));
-        list.add(new Sentence("I am 24 years old."));
-        list.add(new Sentence("I have an acute pain in my stomach accompanied by dysentery."));
-        list.add(new Sentence("There is pain in my lower back-bone."));
-        list.add(new Sentence("I've also vomited a couple of times."));
-        list.add(new Sentence("I had a 100F fever last night, so I took paracitamol to bring the fever down."));
+        list.add("My name is Shubham Dutta.");
+        list.add("I am 24 years old.");
+        list.add("I have an acute pain in my stomach accompanied by dysentery.");
+        list.add("There is pain in my lower back-bone.");
+        list.add("I've also vomited a couple of times.");
+        list.add("I had a 100F fever last night, so I took paracitamol to bring the fever down.");
 
         Assert.assertEquals(list, sentenceSplitter.splitDocument(input));
     }
