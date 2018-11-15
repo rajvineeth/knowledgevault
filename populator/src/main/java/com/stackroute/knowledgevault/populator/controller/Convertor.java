1a512cb25054d36094aef0ec10d6a7f941713def
ShreelekhyaG <shreelu_gampa@yahoo.com>
2018-11-15 11:17:57 +0530

    mapping to existing node

--- populator/src/main/java/com/stackroute/knowledgevault/populator/controller/Convertor.java
+++ populator/src/main/java/com/stackroute/knowledgevault/populator/controller/Convertor.java
@@ -31,7 +31,11 @@ public class Convertor {
     private SymptomService symptomService;
     @Autowired
     private TreatmentService treatmentService;
-
+    static Long symptomId=0L;
+    static Long anatomyId=1L;
+    static Long treatmentId=0L;
+    static Long causeId=0L;
+    static Long id=1L;
     @GetMapping("/{input}")
     public void parse(@PathVariable(value="input") String input) throws IOException {
         ResponseEntity responseEntity;
@@ -40,76 +44,108 @@ public class Convertor {
         Map context = new HashMap();
         JsonLdOptions options = new JsonLdOptions();
         Object compact = JsonLdProcessor.compact(jsonObject, context, options);
-        System.out.println(JsonUtils.toPrettyString(compact));
         String symptomName="",symptomType="";
-        Long symptomId=0L;
+        boolean anatomyPresent=false,causePresent=false,conditionPresent=false,symptomPresent=false,treatmentPresent=false;
+        Anatomy anatomy=null;
         Map<String, Object> root = (Map) jsonObject;
         String type=(String) root.get("@type");
         String name=(String)root.get("alternateName");
         Map<String,String> anatomyMap= (Map<String, String>) root.get("associatedAnatomy");
         String anatomyName=anatomyMap.get("name");
         String anatomyType=anatomyMap.get("@type");
-        Long anatomyId=1L;
-        Anatomy anatomy=new Anatomy(anatomyId,anatomyType,anatomyName);
-        anatomyService.saveAnatomy(anatomy);
+        List<Anatomy> anatomyList=anatomyService.anatomyList();
+        for(Anatomy anatomy1:anatomyList){
+            if(anatomy1.getAnatomyName().equals(anatomyName)){
+                anatomy=anatomy1;
+                anatomyPresent=true;
+            }
+        }
+        if(!anatomyPresent){
+            anatomy=new Anatomy(anatomyId++,anatomyType,anatomyName);
+            anatomyService.saveAnatomy(anatomy);
+        }
         Map<String,String> codeMap= (Map<String, String>) root.get("code");
         Map<String,List> differentialDiagnosis= (Map<String, List>) root.get("differentialDiagnosis");
         List<Map<String,String>> distinguishingSignList=differentialDiagnosis.get("distinguishingSign");
         Iterator signIterator = distinguishingSignList.iterator();
         List<Symptom> symptomList=new ArrayList<>();
         while(signIterator.hasNext()){
+            symptomPresent=false;
             Map<String,String> sym= (Map<String, String>) signIterator.next();
             symptomName=sym.get("name");
             symptomType=sym.get("@type");
-            symptomId++;
-            Symptom symptom=new Symptom(symptomId,symptomType,symptomName);
-            symptomService.saveSymptom(symptom);
-            symptomList.add(symptom);
+            List<Symptom> symptomList1=symptomService.symptomList();
+            for(Symptom symptom:symptomList1){
+                if(symptom.getSymptomName().equals(symptomName)){
+                    symptomList.add(symptom);
+                    symptomPresent=true;
+                }
+            }
+            if(!symptomPresent) {
+                symptomId++;
+                Symptom symptom = new Symptom(symptomId, symptomType, symptomName);
+                symptomService.saveSymptom(symptom);
+                symptomList.add(symptom);
+            }
         }
 
         List<Map<String,Object>> treatment= (List<Map<String, Object>>) root.get("possibleTreatment");
         Iterator treatmentIterator=treatment.iterator();
         String treatmentName="",treatmentType="";
-        Long treatmentId=0L;
+
         List<Treatment> treatmentList=new ArrayList<>();
         while(treatmentIterator.hasNext()){
+            treatmentPresent=false;
             Map<String,Object> stringObjectMap= (Map<String, Object>) treatmentIterator.next();
             treatmentName= (String) stringObjectMap.get("name");
             treatmentType= (String) stringObjectMap.get("@type");
-            treatmentId++;
-            ArrayList<Map<String,String>> dose= (ArrayList<Map<String, String>>) stringObjectMap.get("DoseSchedule");
-            String doseUnit=dose.get(0).get("doseUnit");
-            String frequency=dose.get(1).get("frequency");
-            Treatment treatment1=new Treatment(treatmentId,treatmentType,treatmentName,doseUnit,frequency);
-            treatmentService.saveTreatment(treatment1);
-            treatmentList.add(treatment1);
+            List<Treatment> treatmentList1=treatmentService.treatmentList();
+            for(Treatment treatment1:treatmentList1){
+                if(treatment1.getTreatmentName().equals(treatmentName)){
+                    treatmentList.add(treatment1);
+                    treatmentPresent=true;
+                }
+            }
+            if(!treatmentPresent) {
+                treatmentId++;
+                ArrayList<Map<String, String>> dose = (ArrayList<Map<String, String>>) stringObjectMap.get("DoseSchedule");
+                String doseUnit = dose.get(0).get("doseUnit");
+                String frequency = dose.get(1).get("frequency");
+                Treatment treatment1 = new Treatment(treatmentId, treatmentType, treatmentName, doseUnit, frequency);
+                treatmentService.saveTreatment(treatment1);
+                treatmentList.add(treatment1);
+            }
         }
 
-        Long id=1L;
+
 
         ArrayList<Map<String, String>> causeList = (ArrayList<Map<String, String>>) root.get("cause");
         Iterator mapIterator = causeList.iterator();
         String causeName="",causeType="";
-        Long causeId=0L;
+
         List<Cause> causeList1=new ArrayList<>();
+
         while (mapIterator.hasNext()) {
+            causePresent=false;
             Map<String,String> causeMap = (Map<String, String>) mapIterator.next();
             causeName=(String)causeMap.get("name");
             List<Cause> causeList2=causeService.causeList();
             for(Cause cause:causeList2){
                 if(cause.getCauseName().equals(causeName)){
                     causeList1.add(cause);
-                    causeId++;
-                    continue;
+                    causePresent=true;
                 }
             }
-            causeType = (String) causeMap.get("@type");
-            causeId++;
-            Cause cause=new Cause(causeId,causeType,causeName);
-            causeService.saveCause(cause);
-            causeList1.add(cause);
+            if(!causePresent) {
+                causeType = (String) causeMap.get("@type");
+                causeId++;
+                Cause cause = new Cause(causeId, causeType, causeName);
+                causeService.saveCause(cause);
+                causeList1.add(cause);
+            }
         }
-        medicalConditionService.saveCondition(new MedicalCondition(id,type,name,causeList1,anatomy,symptomList,treatmentList));
+        medicalConditionService.saveCondition(new MedicalCondition(id++,type,name,causeList1,anatomy,symptomList,treatmentList));
+        System.out.println(medicalConditionService.conditionList());
     }
 
 }
