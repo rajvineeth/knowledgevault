00814a5f2a221ba60f0a5a8115467040de8d3e24
ShreelekhyaG <shreelu_gampa@yahoo.com>
2018-11-16 14:33:43 +0530

    mm

--- extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/ExtractorServiceApplication.java
+++ extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/ExtractorServiceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.knowledgevault.extractorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExtractorServiceApplication {
 
 	public static void main(String[] args) {
