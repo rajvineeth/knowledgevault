def2ba1b14e9871ec36f49ffc1907c1d804c3070
Paurush Choudhary <paurushchoudhary496@gmail.com>
2018-11-19 20:17:04 +0530

    working docker-compose

--- paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/ParagraphTokenizerApplication.java
+++ paragraph-tokenizer/src/main/java/com/stackroute/knowledgevault/paragraphtokenizer/ParagraphTokenizerApplication.java
@@ -2,6 +2,7 @@ package com.stackroute.knowledgevault.paragraphtokenizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class ParagraphTokenizerApplication {
