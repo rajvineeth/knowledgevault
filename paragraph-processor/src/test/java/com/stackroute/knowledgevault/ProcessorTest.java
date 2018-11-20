4fbc755613cd410ea0b19b9fd49c04e15c9971f1
jsHyperX <vikramsaurabh14@gmaiil.com>
2018-11-20 18:02:08 +0530

    did a few modifications in paraprocessor

--- paragraph-processor/src/test/java/com/stackroute/knowledgevault/ProcessorTest.java
+++ paragraph-processor/src/test/java/com/stackroute/knowledgevault/ProcessorTest.java
@@ -1,17 +1,18 @@
 package com.stackroute.knowledgevault;
 
 import com.stackroute.knowledgevault.paragraphprocessor.Processor;
-import com.stackroute.knowledgevault.paragraphprocessor.algos.FullTextSearch;
-import com.stackroute.knowledgevault.paragraphprocessor.algos.FullTextSearchImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.*;
 
 public class ProcessorTest {
 
     private Processor processor;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessorTest.class);
 
     @Before
     public void setUp() {
@@ -25,15 +26,20 @@ public class ProcessorTest {
 
     @Test
     public void paraProcessingTest() {
-        String paragraph = "ionizing radiation is not a particularly strong mutagen.[52] Residential exposure to radon gas, for example, has similar cancer risks as passive smoking.[52] Radiation is a more potent source of cancer when combined with other cancer-causing agents, such as radon plus tobacco smoke.[52] Radiation can cause cancer in most parts of the body, in all animals and at any age. Children and adolescents are twice as likely to develop radiation-induced leukemia as adults; radiation exposure before birth has ten times the effect";
-        paragraph.toLowerCase();
-        System.out.println(paragraph);
+        String paragraph = "my name is neeraj and i have been suffering from cancer in my lungs. I also have toothache for past 5days";
+        LOGGER.info(paragraph);
         this.processor.paraProcessing(paragraph);
     }
 
     @Test
     public void keywordMapping() {
         String tag = this.processor.keywordMapping("cancer");
-        assertEquals("disease",tag);
+        assertEquals("diseases",tag);
+    }
+
+    @Test
+    public void testPatternMatch() {
+        String dur = "5days";
+        assertEquals("duration",this.processor.patMatch(dur));
     }
 }
