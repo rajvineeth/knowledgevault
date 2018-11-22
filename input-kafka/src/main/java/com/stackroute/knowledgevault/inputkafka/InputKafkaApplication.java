0efd31aaf90e562c789aad2cd8226776fbd9562d
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-22 14:44:49 +0530

    added microservice to put the input in the kafka server

---
+++ input-kafka/src/main/java/com/stackroute/knowledgevault/inputkafka/InputKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.knowledgevault.inputkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputKafkaApplication.class, args);
+	}
+}
