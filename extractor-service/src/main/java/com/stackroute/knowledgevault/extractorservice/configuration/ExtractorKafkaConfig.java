870a466008de4f90b46ed2a704daf48b2a364089
Paurush Choudhary <paurushchoudhary496@gmail.com>
2018-11-22 15:25:05 +0530

    removed duplicate dependencies and plugins

--- extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/configuration/ExtractorKafkaConfig.java
+++ extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/configuration/ExtractorKafkaConfig.java
@@ -11,7 +11,6 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Configuration
