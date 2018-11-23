e497be1b1c864b4aa26fdf20c83e5cb8c3e767cc
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-23 12:08:27 +0530

    Logger error solved

--- input-postag/src/main/java/com/stackroute/knowledgevault/inputpostag/communicator/KafkaConsumer.java
+++ input-postag/src/main/java/com/stackroute/knowledgevault/inputpostag/communicator/KafkaConsumer.java
@@ -4,25 +4,26 @@ import com.stackroute.knowledgevault.domain.InputPOS;
 import com.stackroute.knowledgevault.domain.UserInput;
 import edu.stanford.nlp.simple.Document;
 import edu.stanford.nlp.simple.Sentence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Service
 public class KafkaConsumer {
 
-    private Logger logger;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
     @Autowired
     private KafkaProducer kafkaProducer;
 
     @KafkaListener(topics="user-input-POS", groupId = "group_json", containerFactory= "documentKafkaListenerFactory")
     public void consumejson(UserInput userInput){
-        logger.info("consumed message");
+        LOGGER.info("consumed message");
 
         Document fileinput = new Document(userInput.getText());
         List<String> POSTags = new ArrayList<>();
@@ -33,6 +34,6 @@ public class KafkaConsumer {
             }
         }
 
-        logger.info(kafkaProducer.post(new InputPOS(userInput.getId(), POSTags)));
+        LOGGER.info(kafkaProducer.post(new InputPOS(userInput.getId(), POSTags)));
     }
 }
