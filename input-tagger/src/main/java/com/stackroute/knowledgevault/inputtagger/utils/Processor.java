5ac2db790b47e7daf56a2401fdda2689db53850d
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-11-23 14:42:04 +0530

    modified processor

--- input-tagger/src/main/java/com/stackroute/knowledgevault/inputtagger/utils/Processor.java
+++ input-tagger/src/main/java/com/stackroute/knowledgevault/inputtagger/utils/Processor.java
@@ -1,12 +1,19 @@
 package com.stackroute.knowledgevault.inputtagger.utils;
 
 import com.stackroute.knowledgevault.domain.InputObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class Processor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Processor.class);
+
     public Map<String, String> process (InputObject inputObject) {
+        LOGGER.info("\n********\nInputObject\n*******\n{}",inputObject.toString());
         return null;
     }
 }
