85c042aaaec250d00aba4c2c907482e5e30e16ab
Paurush Choudhary <paurushchoudhary496@gmail.com>
2018-11-28 19:13:46 +0530

    eureka, config, zuul in docker-compose

--- zuulgateway/src/main/java/com/stackroute/knowledgevault/zuulgateway/ZuulgatewayApplication.java
+++ zuulgateway/src/main/java/com/stackroute/knowledgevault/zuulgateway/ZuulgatewayApplication.java
@@ -6,6 +6,7 @@ import filters.PreFilter;
 import filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulgatewayApplication {
 
 	public static void main(String[] args) {
