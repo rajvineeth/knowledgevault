2a49284e4e7d67b07652eda2e46cb6ff4e2121c1
jsHyperX <vikramsaurabh14@gmaiil.com>
2018-11-28 16:43:43 +0530

    added logo

--- knowledge-vault-ui/src/app/profile/profile.module.ts
+++ knowledge-vault-ui/src/app/profile/profile.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GeneralUserComponent } from './general-user/general-user.component';
 import { SmeModule } from './sme/sme.module';
-import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [GeneralUserComponent, SearchComponent],
+  declarations: [GeneralUserComponent],
   imports: [
     CommonModule,
     FormsModule,
