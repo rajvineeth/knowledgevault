3604dca113dabd2ba9c8b517f69882ab6eeaab89
ShreelekhyaG <shreelu_gampa@yahoo.com>
2018-11-29 09:30:32 +0530

    eureka working

--- paragraph-processor/src/main/java/com/stackroute/knowledgevault/paragraphprocessor/ParagraphProcessorApplication.java
+++ paragraph-processor/src/main/java/com/stackroute/knowledgevault/paragraphprocessor/ParagraphProcessorApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.knowledgevault.paragraphprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ParagraphProcessorApplication {
 
 	public static void main(String[] args) {
--- zuulgateway/src/main/java/com/stackroute/zuulgateway/ZuulgatewayApplication.java
+++ zuulgateway/src/main/java/com/stackroute/zuulgateway/ZuulgatewayApplication.java
@@ -6,11 +6,13 @@ import filters.PreFilter;
 import filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulgatewayApplication {
 
 	public static void main(String[] args) {
