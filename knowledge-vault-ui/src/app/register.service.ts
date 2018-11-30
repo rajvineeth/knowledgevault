81eab5b90c695a9391a72263393d69afaf6adedf
Tushh007 <tushar.langer@gmail.com>
2018-11-30 10:19:02 +0530

    fixed minor errors

---
+++ knowledge-vault-ui/src/app/register.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from './_models';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RegisterService {
+
+  private registerUrl: string = 'http://localhost:8181/api/v1/saveuser';
+
+  constructor(private http: HttpClient) { }
+
+  saveUser(user: User) {
+    this.http.post(this.registerUrl, httpOptions);
+  }
+}
