50f2e9bbfbb1e3aba51bdf728e3f2c005fec8d5b
CGI-AnandJ <anandj709@gmail.com>
2018-11-30 12:39:39 +0530

    Looader

--- query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/QueryEngineApplication.java
+++ query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/QueryEngineApplication.java
@@ -2,12 +2,22 @@ package com.stackroute.knowledgevault.queryengine;
 
 import java.lang.String;
 
+import com.stackroute.knowledgevault.queryengine.service.DriverInit;
+import com.stackroute.knowledgevault.queryengine.service.QueryService;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QueryEngineApplication {
 
-	public static void main(String[] args) { SpringApplication.run(QueryEngineApplication.class, args);
+	public static void main(String[] args) {
+	QueryService queryService=new QueryService();
+	DriverInit driver = new DriverInit("bolt://0.0.0.0:7687", "neo4j", "123456");
+	Driver drive=driver.getDriver();
+	queryService.loadgraph(drive);
+	SpringApplication.run(QueryEngineApplication.class, args);
+
 	}
 }
