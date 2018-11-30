f51bdb34490d3c4f65af9b2046ca89b35af5500d
CGI-AnandJ <anandj709@gmail.com>
2018-11-30 19:31:02 +0530

    One result  working

--- query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/service/QueryService.java
+++ query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/service/QueryService.java
@@ -5,16 +5,18 @@ import org.neo4j.driver.v1.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class QueryService {
     private static final Logger LOGGER = LoggerFactory.getLogger(QueryService.class);
 
-    public OutputResult runquery(Driver driver, String k1, String k2) {
+    public List<OutputResult> runquery(Driver driver, String k1, String k2) {
         LOGGER.info("received keywords from tagger");
         Map<String, String> map = new HashMap<>();
-        OutputResult outputResult = new OutputResult();
+        List<OutputResult> res = new ArrayList();
 
         // Sessions are lightweight and disposable connection wrappers.
         try (Session session = driver.session()) {
@@ -22,6 +24,7 @@ public class QueryService {
             // and makes handling errors much easier.
             String q = "MATCH (A:" + k2 + "),p=(B)-[r]-(A) WHERE A.name contains '" + k1 + "' RETURN A.name As name1, labels(A) as name1label, TYPE(r) as ril, B.name As name2, labels(B) as name2label LIMIT 10";
             try (Transaction tx = session.beginTransaction()) {
+                OutputResult outputResult = new OutputResult();
                 LOGGER.info(q);
                 StatementResult result = tx.run(q);
                 while (result.hasNext()) {
@@ -41,9 +44,10 @@ public class QueryService {
                 LOGGER.info("Out of string loop");
                 System.out.println(outputResult);
                 tx.success();  // Mark this write as successful.
-                return outputResult;
+                res.add(outputResult);
             }
         }
+        return res;
     }
 
     public void loadgraph(Driver driver) {
