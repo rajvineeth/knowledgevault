f8ffa22594f7864aedbde9818b288063c2e3f0ce
CGI-AnandJ <anandj709@gmail.com>
2018-11-30 09:38:55 +0530

    Merge resolved

--- query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/service/QueryService.java
+++ query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/service/QueryService.java
@@ -20,17 +20,22 @@ public class QueryService {
         try (Session session = driver.session()) {
             // Wrapping Cypher in an explicit transaction provides atomicity
             // and makes handling errors much easier.
-            String q = "MATCH (A:MedicalSymptom),(B:MedicalCondition),p=(B)-[r:has_symptom]-(A) WHERE A.name contains '" + k1 + "' RETURN A.name As name1, B.name As name  LIMIT 10";
+            String q = "MATCH (A:" + k2 + "),p=(B)-[r]-(A) WHERE A.name contains '" + k1 + "' RETURN A.name As name1, labels(A) as name1label, TYPE(r) as ril, B.name As name2, labels(B) as name2label LIMIT 10";
             try (Transaction tx = session.beginTransaction()) {
                 LOGGER.info(q);
                 StatementResult result = tx.run(q);
                 while (result.hasNext()) {
                     Record record = result.next();
                     // Values can be extracted from a record by index or name.
-                    map.put(record.get("name").asString(), record.get("name1").asString());
-//                    System.out.println(record.get("name").asString() + " " + record.get("name1").asString());
-                    outputResult.MedicalCondition = record.get("name").asString();
-                    outputResult.MedicalSymptom = record.get("name1").asString();
+//                    String r = record.get("name1label").asString();
+//
+                    outputResult.Node1 = record.get("name1").asString();
+                    outputResult.Node1label = record.get("name1label").asString();
+                    outputResult.Relation = record.get("ril").asString();
+                    outputResult.Node2 = record.get("name2").asString();
+                    outputResult.Node2label = record.get("name2label").asString();
+
+
                 }
                 LOGGER.info("output :{}", map);
                 LOGGER.info("Out of string loop");
