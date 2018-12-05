a531a1bc7c766cf3265bf1bbb21a11f1412e109f
arpitmishra02 <arpitmishra.am02@gmail.com>
2018-12-05 18:38:45 +0530

    query engine

--- query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/QueryEngineApplication.java
+++ query-engine/src/main/java/com/stackroute/knowledgevault/queryengine/QueryEngineApplication.java
@@ -9,8 +9,10 @@ import org.neo4j.driver.v1.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QueryEngineApplication {
 
 	public static void main(String[] args) {
