4f463d0f641d3f6590d7f2226eb8f8318c63ace5
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-12-06 11:51:08 +0530

    added alternate name and method to capitalize 1st letter

--- knowledge-vault-ui/src/app/card/card.component.ts
+++ knowledge-vault-ui/src/app/card/card.component.ts
@@ -10,13 +10,28 @@ import { ReceivedQuery } from '../models/receivedQuery';
 export class CardComponent implements OnInit {
 
   @Input() private card: ReceivedQuery;
+  private name: string;
+  private alternateName: string;
 
   constructor(private router: Router) {
    }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    this.name = this.card.diseaseName.substr(1, this.card.diseaseName.length - 2);
+    const partsOfName = this.name.trim().split('\u000b');
+    if (partsOfName.length > 1) {
+      this.name = partsOfName[0];
+      this.alternateName = partsOfName[1];
+      this.alternateName = this.ucFirst(this.alternateName);
+    }
+    this.name = this.ucFirst(this.name);
+   }
 
   detailedView(card: ReceivedQuery) {
     this.router.navigate(['carddetail/{{card.MedicalCondition}}']);
+  }
+
+  ucFirst(name: string) {
+    return name.charAt(0).toUpperCase() + name.substr(1);
   }
 }
