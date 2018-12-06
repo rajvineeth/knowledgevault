57339880229c16c5727834f85684f85db017b486
arpitmishra02 <arpitmishra.am02@gmail.com>
2018-12-06 21:25:54 +0530

    drap n drop

--- knowledge-vault-ui/src/app/profile/sme/dragndrop/dragndrop.component.ts
+++ knowledge-vault-ui/src/app/profile/sme/dragndrop/dragndrop.component.ts
@@ -31,7 +31,7 @@ export class DragndropComponent implements OnInit {
         this.FileName = this.fileList[i].name;
 //        console.log(this.fileList[i]);
         this.formData.append('File', this.fileList[i]);
-        console.log(this.formData.get('File'));
+//        console.log(this.formData.getAll('File'));
         
         // append more item in FormData and send to server
         // call service to send file on server via FormData.
@@ -39,6 +39,7 @@ export class DragndropComponent implements OnInit {
           // if file saved in DB set true.
         // this.IsUploaded = true;
       }
+      console.log(this.formData.getAll('File'));
       this.upload_service.extractFile(this.formData);
       //console.log(this.formData.getAll('File'));
     }
