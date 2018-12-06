57a929c7a2b6936a9d4eac8ae29d2b306ef30bcb
ShreelekhyaG <shreelu_gampa@yahoo.com>
2018-12-06 11:04:21 +0530

    web-scraping n populator consuming

--- web-scraping/src/main/java/com/stackroute/knowledgevault/webscraping/services/StopwordRemoval.java
+++ web-scraping/src/main/java/com/stackroute/knowledgevault/webscraping/services/StopwordRemoval.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
 public class StopwordRemoval {
 
 
-    private String stopWords="stopwords.txt";
+    private String stopWords="/knowledge-vault/web-scraping/stopwords.txt";
 
     private static Logger logger;
 
