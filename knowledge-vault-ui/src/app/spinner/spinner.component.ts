88e222f3773dfd5f09b27f9382430f82528534bd
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-12-07 10:26:48 +0530

    updated dictionary

---
+++ knowledge-vault-ui/src/app/spinner/spinner.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.css']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
