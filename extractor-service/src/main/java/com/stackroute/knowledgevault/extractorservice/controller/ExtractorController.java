4b3a25a24003d8c2dda374b55864031c37598874
arpitmishra02 <arpitmishra.am02@gmail.com>
2018-12-07 13:12:32 +0530

    reading multipart

--- extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/controller/ExtractorController.java
+++ extractor-service/src/main/java/com/stackroute/knowledgevault/extractorservice/controller/ExtractorController.java
@@ -14,6 +14,7 @@ import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -137,8 +138,11 @@ public class ExtractorController {
         System.out.println("reached");
 
         File convFile = new File( file.getOriginalFilename());
+        convFile.createNewFile();
 
-        file.transferTo(convFile);
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
 
         System.out.println(convFile.canRead() + " name:" + convFile.getName());
 
