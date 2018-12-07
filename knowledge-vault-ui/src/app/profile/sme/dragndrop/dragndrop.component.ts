f59a2be00de165e9faebeb95bd2c248eb33cb79e
jsHyperX <vikramsaurabh14@gmail.com>
2018-12-07 11:06:12 +0530

    testting for cors error

--- knowledge-vault-ui/src/app/profile/sme/dragndrop/dragndrop.component.ts
+++ knowledge-vault-ui/src/app/profile/sme/dragndrop/dragndrop.component.ts
@@ -9,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class DragndropComponent implements OnInit {
 
-  constructor(private upload_service: FileUploadService) {
-  }
+  constructor(private upload_service: FileUploadService) {}
 
   fileList = [];
   FileName: string;
@@ -25,14 +24,15 @@ export class DragndropComponent implements OnInit {
   sendFileDB() {
     this.IsUploaded = false;
     this.show_success_msg = true;
-    this.save_success = "Thank you for your contribution.We have saved your document in our database.We will add it to our knowledge-base once it is approved by Paurush Chaudhary."
+    // tslint:disable-next-line:max-line-length
+    this.save_success = 'Thank you for your contribution.We have saved your document in our database.We will add it to our knowledge-base once it is approved by Paurush Chaudhary.';
     if (this.fileList.length > 0) {
       for (let i = 0; i < this.fileList.length; i++) {
         this.FileName = this.fileList[i].name;
 //        console.log(this.fileList[i]);
         this.formData.append('File', this.fileList[i]);
 //        console.log(this.formData.getAll('File'));
-        
+
         // append more item in FormData and send to server
         // call service to send file on server via FormData.
 
@@ -41,7 +41,7 @@ export class DragndropComponent implements OnInit {
       }
       console.log(this.formData.getAll('File').valueOf());
       this.upload_service.extractFile(this.formData);
-      //console.log(this.formData.getAll('File'));
+      // console.log(this.formData.getAll('File'));
     }
   }
   getFiles(files) {
